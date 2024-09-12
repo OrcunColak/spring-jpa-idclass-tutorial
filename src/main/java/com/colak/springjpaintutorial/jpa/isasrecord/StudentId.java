531b4beef3dd60c53de0046bbe7223c2ca6645a4
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-12 09:49:47 +0300

    id class as record example

--- src/main/java/com/colak/springjpaintutorial/jpa/isasrecord/StudentId.java
+++ src/main/java/com/colak/springjpaintutorial/jpa/isasrecord/StudentId.java
@@ -1,6 +1,13 @@
 package com.colak.springjpaintutorial.jpa.isasrecord;
 
+// When Hibernate instantiate a new IdClass record, its default EmbeddableInstantiator assigns the values of
+// the primary key attributes in the alphabetical order of their attribute names.
+//
+// Therefore, if the attributes of the record are not arranged in alphabetical order, the mapping between the record class attributes and
+// the primary key attributes assigned by the EmbeddableInstantiator will be misaligned.
+// This will cause the findById method to function incorrectly.
 public record StudentId(String address,
                         Integer age,
                         String name
-) {}
+) {
+}
